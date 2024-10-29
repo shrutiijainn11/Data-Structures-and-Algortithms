fc8f4c0ce6a89945939a1b281331df78d78de30f
Shruti Jain <158989102+shrutiijainn11@users.noreply.github.com>
2024-10-29 12:47:55 +0530

    Add files via upload

---
+++ FloydWarshall.java
@@ -0,0 +1,50 @@
+class FloydWarshall {
+    final static int INF = 99999;
+
+    void floydWarshall(int graph[][]) {
+        int V = graph.length;
+        int[][] dist = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        printSolution(dist);
+    }
+
+    void printSolution(int dist[][]) {
+        int V = dist.length;
+        System.out.println("Following matrix shows the shortest distances between every pair of vertices:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF) {
+                    System.out.print("INF ");
+                } else {
+                    System.out.print(dist[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int graph[][] = new int[][] {
+            {0, 5, INF, 10},
+            {INF, 0, 3, INF},
+            {INF, INF, 0, 1},
+            {INF, INF, INF, 0}
+        };
+        FloydWarshall fw = new FloydWarshall();
+        fw.floydWarshall(graph);
+    }
+}
